79072bc9daa9bb0cc48d58ab07549957667dc4f5
Yaroslavna Strogova <un.spezials@mail.ru>
2023-03-05 12:31:52 +0300

    fix: исправить замечания после 1 ревью

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,30 @@
-//открытие и закрытие попап
 let editButton = document.querySelector('.profile-info__edit-btn');
 let popup = document.querySelector('.popup');
 let closeButton = popup.querySelector('.popup__close-btn');
 
+let formElement = document.querySelector('.popup__form');
+let nameInput = formElement.querySelector('.popup__input_edit_name');
+let jobInput = formElement.querySelector('.popup__input_edit_job');
+let profileName = document.querySelector('.profile-info__name');
+let profileAboutMe = document.querySelector('.profile-info__about-me');
+
+//открытие и закрытие попап
+
 const openPopup = function(){
   popup.classList.add('popup_opened');
+  nameInput.value = profileName.textContent;
+  jobInput.value = profileAboutMe.textContent;
 }
-
-editButton.addEventListener('click', openPopup);
 
 const closePopup = function(){
   popup.classList.remove('popup_opened');
 }
 
+editButton.addEventListener('click', openPopup);
+
 closeButton.addEventListener('click', closePopup);
 
 //Редактирование имени и информации о себе
-
-let formElement = document.querySelector('.form');
-let nameInput = formElement.querySelector('.popup__name');
-let jobInput = formElement.querySelector('.popup__info');
-let profileName = document.querySelector('.profile-info__name');
-let profileAboutMe = document.querySelector('.profile-info__about-me');
 
 function handleFormSubmit (evt) {
     evt.preventDefault();
